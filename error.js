0b1ccce2e0b197da11259f082559e8024be32ba8
Ibby Hadeed <ibby93@gmail.com>
2016-07-06 05:05:10 -0400

    refractor(): semi columns and error function

--- error.js
+++ error.js
@@ -1,14 +1,14 @@
-var util = require('util')
+var util = require('util');
 
 module.exports = function UnauthorizedError (code, error) {
-  Error.captureStackTrace(this, this.constructor)
+  Error.captureStackTrace(this, this.constructor);
 
-  this.name = this.constructor.name
-  this.message = error.message
+  this.name = this.constructor.name;
+  this.message = error;
 
-  this.code = code
-  this.status = 403
-  this.inner = error
-}
+  this.code = code;
+  this.status = 403;
+  this.inner = {message: error};
+};
 
-util.inherits(module.exports, Error)
+util.inherits(module.exports, Error);
