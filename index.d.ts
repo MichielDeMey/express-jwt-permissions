93a81c5246479e1d81e5010e9123c184e0cf89e7
Yacov <yacov@yacov.yacov>
2021-08-03 18:46:07 +0300

    Support unless in type definition

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,19 @@
-import { Handler } from 'express'
+import express = require('express');
+import unless = require('express-unless');
 
 declare interface GuardOptions {
   requestProperty?: string
   permissionsProperty?: string
 }
 
+interface RequestHandlerEx extends express.RequestHandler {
+  unless: typeof unless;
+}
+
 declare class Guard {
   public constructor(options?: GuardOptions);
 
-  public check(required: string | string[] | string[][]): Handler;
+  public check(required: string | string[] | string[][]): RequestHandlerEx;
 }
 
 declare function guardFactory(options?: GuardOptions): Guard;
